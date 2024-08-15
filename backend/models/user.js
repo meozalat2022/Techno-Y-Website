90843f32ff201f99bef0f5ef91278e43bfa7d1e0
meozalat2022 <meozalat2002@hotmail.com>
2024-08-15 15:59:55 +0300

    signup user

--- backend/models/user.js
+++ backend/models/user.js
@@ -20,7 +20,7 @@ const userSchema = new mongoose.Schema({
     default: "",
   },
   phone: {
-    type: String,
+    type: Number,
     required: true,
   },
   address: {
--- src/app/signIn/page.jsx
+++ src/app/signIn/page.jsx
@@ -9,6 +9,9 @@ import {
 import { useRouter } from "next/navigation";
 import { useDispatch, useSelector } from "react-redux";
 import Link from "next/link";
+import { LockOutlined, UserOutlined } from "@ant-design/icons";
+import { Button, Checkbox, Form, Input, Flex } from "antd";
+
 const SignIn = () => {
   const [formData, setFormData] = useState({});
 
@@ -18,49 +21,66 @@ const SignIn = () => {
 
   const dispatch = useDispatch();
 
-  const onChangeHandler = (e) => {
-    setFormData({
-      ...formData,
-      [e.target.id]: e.target.value,
-    });
+  const onFinish = (values) => {
+    console.log("Received values of form: ", values);
+    try {
+    } catch (error) {}
   };
 
-  const onSubmitHandler = (e) => {
-    console.log(e);
-  };
   return (
-    <div className="p-4 max-w-lg mx-auto">
-      <h1 className="text-3xl font-semibold text-center my-7">تسجيل الدخول</h1>
-      <form onSubmit={onSubmitHandler} className="flex flex-col gap-4">
-        <input
-          onChange={onChangeHandler}
-          type="text"
-          placeholder="اسم المستخدم"
-          id="username"
-          className="border p-3 rounded-lg text-right"
-        />
-        <input
-          onChange={onChangeHandler}
-          type="password"
-          placeholder="الرقم السري"
-          id="password"
-          className="border p-3 rounded-lg text-right"
-        />
-        <button
-          disabled={loading}
-          className="bg-slate-700 p-3 text-white hover:opacity-90 disabled:opacity-80 rounded-lg uppercase "
+    <div className="m-10 flex justify-center items-center">
+      <Form
+        name="login"
+        initialValues={{
+          remember: true,
+        }}
+        style={{
+          maxWidth: 360,
+        }}
+        onFinish={onFinish}
+      >
+        <Form.Item
+          name="username"
+          rules={[
+            {
+              required: true,
+              message: "Please input your Username!",
+            },
+          ]}
         >
-          {loading ? "تحميل..." : "تسجيل الدخول"}
-        </button>
-        {/* <GoogleAuth /> */}
-      </form>
-      <div className="flex gap-3 mt-5 justify-end">
-        <Link href={"/signUp"}>
-          <span className="text-blue-500 text-right">انشاء حساب جديد</span>
-        </Link>
-        <p>ليس لديك جساب؟</p>
-      </div>
-      {error && <p className="text-red-500 mt-5">{error}</p>}
+          <Input prefix={<UserOutlined />} placeholder="Username" />
+        </Form.Item>
+        <Form.Item
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: "Please input your Password!",
+            },
+          ]}
+        >
+          <Input
+            prefix={<LockOutlined />}
+            type="password"
+            placeholder="Password"
+          />
+        </Form.Item>
+        <Form.Item>
+          <Flex justify="space-between" align="center">
+            <Form.Item name="remember" valuePropName="checked" noStyle>
+              <Checkbox>Remember me</Checkbox>
+            </Form.Item>
+            <a href="">Forgot password</a>
+          </Flex>
+        </Form.Item>
+
+        <Form.Item>
+          <Button block type="primary" htmlType="submit">
+            Log in
+          </Button>
+          or <a href="">Register now!</a>
+        </Form.Item>
+      </Form>
     </div>
   );
 };
